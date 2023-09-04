31bde54ac649e20e02dc2f020066b61ddfb8a68e
Areeba Shakeel <shakeelareeba02>
2023-09-04 22:57:31 +0530

    post_page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import React from 'react'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import './App.css';
+import Post from './component/post';
+
+function App() {
+  return (
+   <div className="">
+      <Post /> 
+   </div>
+  );
+}
+
+export default App;
